f11b28689adde97b3b6f6abeae034645c2cd7ce4
Vishwesh Bhat <54475954+vishwesh2000@users.noreply.github.com>
2023-03-03 21:03:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,14 +31,6 @@ def save_uploadedfile(uploadedfile):
     with open(uploadedfile.name,"wb") as f:
         f.write(uploadedfile.getbuffer())
 
-stability_api = client.StabilityInference(
-    key=st.secrets["STABILITY_KEY"], #os.environ("STABILITY_KEY"), # key=os.environ['STABILITY_KEY'], # API Key reference.
-    verbose=True, # Print debug messages.
-    engine="stable-diffusion-v1-5", # Set the engine to use for generation.
-    # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
-    # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
-)
-
 header = ["sl. no.", "Input Prompt", "Output", "Date_time"]
 
 def csv_logs(mytext, result, date_time):
@@ -283,37 +275,7 @@ if Input_type == 'TEXT':
                     date_time = now.strftime("%Y-%m-%d %H:%M:%S")
                     csv_logs(mytext, rx, date_time)
             except:
-                # Set up our initial generation parameters.
-                answers = stability_api.generate(
-                prompt = mytext,
-                seed=992446758, # If a seed is provided, the resulting generated image will be deterministic.
-                        # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
-                        # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
-                steps=30, # Amount of inference steps performed on image generation. Defaults to 30.
-                cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
-                    # Setting this value higher increases the strength in which it tries to match your prompt.
-                    # Defaults to 7.0 if not specified.
-                width=512, # Generation width, defaults to 512 if not included.
-                height=512, # Generation height, defaults to 512 if not included.
-                samples=1, # Number of images to generate, defaults to 1 if not included.
-                sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
-                                                    # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
-                                                    # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
-                )
-
-                for resp in answers:
-                    for artifact in resp.artifacts:
-                        if artifact.finish_reason == generation.FILTER:
-                            warnings.warn(
-                                "Your request activated the API's safety filters and could not be processed."
-                                "Please modify the prompt and try again.")
-                        if artifact.type == generation.ARTIFACT_IMAGE:
-                            img = Image.open(io.BytesIO(artifact.binary))
-                            st.image(img)
-                            img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
-                            rx = 'Image returned'
-                            # g_sheet_log(mytext, rx)
-                            csv_logs(mytext, rx, date_time)
+                pass
                             
 
         elif ("vid_tube" in string_temp):
@@ -425,37 +387,7 @@ elif Input_type == 'SPEECH':
                             date_time = now.strftime("%Y-%m-%d %H:%M:%S")
                             csv_logs(mytext, rx, date_time)
                     except:
-                        # Set up our initial generation parameters.
-                        answers = stability_api.generate(
-                        prompt = mytext,
-                        seed=992446758, # If a seed is provided, the resulting generated image will be deterministic.
-                                # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
-                                # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
-                        steps=30, # Amount of inference steps performed on image generation. Defaults to 30.
-                        cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
-                            # Setting this value higher increases the strength in which it tries to match your prompt.
-                            # Defaults to 7.0 if not specified.
-                        width=512, # Generation width, defaults to 512 if not included.
-                        height=512, # Generation height, defaults to 512 if not included.
-                        samples=1, # Number of images to generate, defaults to 1 if not included.
-                        sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
-                                                            # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
-                                                            # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
-                        )
-            
-                        for resp in answers:
-                            for artifact in resp.artifacts:
-                                if artifact.finish_reason == generation.FILTER:
-                                    warnings.warn(
-                                        "Your request activated the API's safety filters and could not be processed."
-                                        "Please modify the prompt and try again.")
-                                if artifact.type == generation.ARTIFACT_IMAGE:
-                                    img = Image.open(io.BytesIO(artifact.binary))
-                                    st.image(img)
-                                    img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
-                                    rx = 'Image returned'
-                                    # g_sheet_log(mytext, rx)
-                                    csv_logs(mytext, rx, date_time)
+                        pass
                     
                 elif ("vid_tube" in string_temp):
                     s = Search(question)
