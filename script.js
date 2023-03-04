bba4b09edccf8b102bc1be3308c280dcb00b3581
Vishwesh Bhat <54475954+vishwesh2000@users.noreply.github.com>
2023-03-04 10:46:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // script.js
 
-window.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("myIframe").src = "https://cobaltzvc-hyperbot.hf.space";
-});
+// Define the URL of the iframe
+var srcUrl = "https://cobaltzvc-hyperbot.hf.space";
+
+// Define a function to set the src attribute of the iframe element
+function setIframeSrc() {
+  document.getElementById("myIframe").src = srcUrl;
+}
+
+window.addEventListener("DOMContentLoaded", setIframeSrc);
